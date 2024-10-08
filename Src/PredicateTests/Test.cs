15d7589c87865657ce340401ee36d9f2542079ac
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-08 23:57:58 +0100

    feat - Renamed project and added new classes and functionality.

---
+++ Src/PredicateTests/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace POCPredicate;
+
+public static class Test
+{
+    public static void Resolve<T>(Expression<Func<T, bool>> predicate)
+        where T : class, IEntity, new()
+    {
+        var param = predicate.Parameters[0];
+        var operation = (BinaryExpression)predicate.Body;
+        var left = (ParameterExpression)operation.Left;
+        var right = (ConstantExpression)operation.Right;
+
+        Console.WriteLine(
+            "Decomposed expression: {0} => {1} {2} {3}",
+            param.Name,
+            left.Name,
+            operation.NodeType,
+            right.Value
+        );
+    }
+}
---
+++ Src/PredicateTests/Teste.cs
@@ -0,0 +1,10 @@
+﻿namespace POCPredicate;
+
+public class Teste : IEntity
+{
+    public bool IsActive { get; set; }
+
+    public string Document { get; set; }
+
+    public string Name { get; set; }
+}
