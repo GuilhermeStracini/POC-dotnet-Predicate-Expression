e71dabc9ab40a25ab08af884b3707c315b74a824
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-08 23:17:30 +0100

    feat - Add configuration for dotnet-tools and pre-commit hook.

--- PredicateTests/Program.cs
+++ PredicateTests/Program.cs
@@ -10,16 +10,13 @@ namespace PredicateTests
             Console.WriteLine("Hello World!");
 
             Test.Resolve<Teste>(t =>
-                t.IsActive && t.Name == "Test" && t.Document == "1" ||
-                t.Name == "or" && t.Document == "2" ||
-                t.Document == "3" ||
-                !t.IsActive);
-
+                t.IsActive && t.Name == "Test" && t.Document == "1"
+                || t.Name == "or" && t.Document == "2"
+                || t.Document == "3"
+                || !t.IsActive
+            );
         }
-
-
     }
-
 
     public interface IEntity { }
 
@@ -34,16 +31,21 @@ namespace PredicateTests
 
     public static class Test
     {
-        public static void Resolve<T>(Expression<Func<T, bool>> predicate) where T : class, IEntity, new()
+        public static void Resolve<T>(Expression<Func<T, bool>> predicate)
+            where T : class, IEntity, new()
         {
             var param = (ParameterExpression)predicate.Parameters[0];
             var operation = (BinaryExpression)predicate.Body;
             var left = (ParameterExpression)operation.Left;
             var right = (ConstantExpression)operation.Right;
 
-            Console.WriteLine("Decomposed expression: {0} => {1} {2} {3}",
-                param.Name, left.Name, operation.NodeType, right.Value);
+            Console.WriteLine(
+                "Decomposed expression: {0} => {1} {2} {3}",
+                param.Name,
+                left.Name,
+                operation.NodeType,
+                right.Value
+            );
         }
     }
 }
-
